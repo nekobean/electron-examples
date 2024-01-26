997482b7025e0d28f78e5ffcf34aee24a19bc303
nekobean <nobepon@gmail.com>
2024-01-26 18:57:05 +0900

    一部修正

--- 04_electron-tutorial-hot-reload/javascript/main.cjs
+++ 04_electron-tutorial-hot-reload/javascript/main.cjs
@@ -2,11 +2,14 @@ const { app, BrowserWindow } = require("electron");
 const electronReload = require("electron-reload");
 const path = require("path");
 
+// 開発環境かどうか
+const idDev = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+
 const createWindow = () => {
   const win = new BrowserWindow();
   win.loadFile("index.html");
 
-  if (!app.isPackaged) {
+  if (idDev) {
     // パッケージ化されていない場合
     const electronPath = path.join(
       __dirname,
--- 04_electron-tutorial-hot-reload/typescript/main.ts
+++ 04_electron-tutorial-hot-reload/typescript/main.ts
@@ -1,11 +1,14 @@
 import { app, BrowserWindow } from "electron";
 import * as path from "path";
 
+// 開発環境かどうか
+const idDev = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+
 const createWindow = () => {
   const win = new BrowserWindow();
   win.loadFile("index.html");
 
-  if (!app.isPackaged) {
+  if (idDev) {
     // バイナリ化されていない場合
     const electronPath = path.join(
       __dirname,
