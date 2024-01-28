2181630173f064ef1e57831fe59665cbce40d7a0
nekobean <nobepon@gmail.com>
2024-01-28 21:07:53 +0900

    build/ を削除

--- 10_electron-load-config/build/preload.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron_1 = require("electron");
-electron_1.contextBridge.exposeInMainWorld("Electron", {
-    send: () => electron_1.ipcRenderer.sendSync("getConfig"),
-});
